0e4643d1a82d01b9c262f1e9eb2509a1a84ed14e
Charles-IV <cthefourth@gmail.com>
2017-05-20 09:04:57 +0100

    initial commit

---
+++ 4034X-vendor-blobs.mk
@@ -0,0 +1,15 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/data,system/data)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tcl/4034X/wlan,system/wlan)
